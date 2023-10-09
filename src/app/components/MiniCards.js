b62a281602cc810abdadf6e7ba33f48c6f3bb4a6
w.dewi@global-coding.com <w.dewi@global-coding.com>
2023-10-09 15:04:30 +0700

    feature-bug-fix

--- src/app/components/MiniCards.js
+++ src/app/components/MiniCards.js
@@ -3,10 +3,10 @@ import React, { useContext } from "react";
 const MiniCards = (props) => {
 
   return (
-    <div className='bg-white flex'>
-        <div className="w-96">
-            <p className="text-black">{props.description}</p>
-            <p className="text-black">{props.name}</p>
+    <div className='bg-white flex rounded-lg'>
+        <div className="w-96 px-4 text-sm font-poppins not-italic">
+            <p className="text-black py-3.5 items-justify">{props.description}</p>
+            <p className="text-black pb-3 text-teal-400">{props.name}</p>
         </div>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Image from 'next/image';
 
 import '../app/styles/pages.css';
 import Button from './components/Button';
-import Top from '../app/assets/background_top.png';
 import Iphone from '../app/assets/iphone.png';
 import Minimo from '../app/assets/mini-mobile.png';
 import Minip from '../app/assets/mini-pc.png';
@@ -52,7 +51,7 @@ const Home = () => {
     <main className="flex min-h-screen flex-col bg-black">
 
       {/* //section-1// */}
-      <div>
+      <div className='bg-top'>
         <svg xmlns="http://www.w3.org/2000/svg" width="37" height="37" viewBox="0 0 37 37" fill="none">
           <g clip-path="url(#clip0_0_27)">
             <path d="M12.7583 13.1223L7.66914 15.5411L10.9665 20.1103C11.0763 20.2599 11.1555 20.4298 11.1993 20.6102C11.2432 20.7905 11.2509 20.9778 11.2221 21.1611C11.1932 21.3445 11.1283 21.5203 11.0312 21.6785C10.934 21.8367 10.8065 21.974 10.656 22.0827C10.5055 22.1913 10.335 22.2691 10.1543 22.3115C9.97359 22.3539 9.78628 22.3602 9.60316 22.3298C9.42003 22.2995 9.24473 22.2332 9.08734 22.1348C8.92996 22.0364 8.79362 21.9078 8.68618 21.7564L4.4017 15.8187C4.28102 15.6515 4.19858 15.4599 4.16022 15.2573C4.12187 15.0547 4.12853 14.8461 4.17974 14.6464C4.23096 14.4467 4.32546 14.2606 4.45657 14.1015C4.58768 13.9423 4.75218 13.814 4.93842 13.7255L11.5514 10.582C11.8878 10.4255 12.2724 10.4082 12.6214 10.534C12.9705 10.6598 13.2557 10.9184 13.4149 11.2535C13.5741 11.5886 13.5944 11.9731 13.4714 12.3231C13.3485 12.6731 13.0921 12.9604 12.7583 13.1223ZM32.1095 20.6924L27.825 14.7547C27.7175 14.6033 27.5812 14.4747 27.4238 14.3763C27.2664 14.2779 27.0911 14.2116 26.908 14.1813C26.7249 14.151 26.5376 14.1572 26.3569 14.1996C26.1762 14.242 26.0056 14.3198 25.8551 14.4284C25.7046 14.5371 25.5771 14.6744 25.48 14.8326C25.3828 14.9908 25.318 15.1666 25.2891 15.35C25.2602 15.5333 25.2679 15.7206 25.3118 15.9009C25.3557 16.0813 25.4348 16.2512 25.5447 16.4008L28.842 20.97L23.7529 23.3888C23.5846 23.4671 23.4335 23.578 23.3082 23.715C23.183 23.852 23.0861 24.0124 23.0232 24.1871C22.9603 24.3617 22.9325 24.547 22.9416 24.7324C22.9506 24.9178 22.9963 25.0996 23.076 25.2673C23.1556 25.4349 23.2677 25.5851 23.4057 25.7093C23.5437 25.8334 23.7049 25.929 23.88 25.9905C24.0551 26.0521 24.2407 26.0783 24.426 26.0678C24.6113 26.0572 24.7927 26.0101 24.9597 25.9291L31.5727 22.7856C31.759 22.6971 31.9235 22.5688 32.0546 22.4096C32.1857 22.2505 32.2802 22.0645 32.3314 21.8647C32.3826 21.665 32.3893 21.4564 32.3509 21.2538C32.3126 21.0513 32.2301 20.8596 32.1095 20.6924ZM25.2432 8.03649C25.0907 7.93222 24.9192 7.85902 24.7384 7.82105C24.5576 7.78309 24.3712 7.78111 24.1896 7.81522C24.0081 7.84934 23.8351 7.91888 23.6804 8.01989C23.5258 8.12089 23.3925 8.25137 23.2883 8.40387L10.9006 26.5198C10.6904 26.8276 10.6111 27.2064 10.68 27.5727C10.749 27.9391 10.9606 28.2631 11.2683 28.4735C11.4032 28.5667 11.5536 28.6352 11.7125 28.6757C11.9921 28.7474 12.2871 28.7314 12.5573 28.6297C12.8275 28.528 13.0599 28.3456 13.2228 28.1072L25.6105 9.99134C25.7148 9.83887 25.788 9.66735 25.826 9.48658C25.8639 9.30582 25.8659 9.11934 25.8318 8.93781C25.7977 8.75628 25.7281 8.58324 25.6271 8.42859C25.5261 8.27394 25.3957 8.1407 25.2432 8.03649Z" fill="white" />
@@ -78,57 +77,59 @@ const Home = () => {
           Create Mobile Applications and Websites for Companies or Your Business<br />
           Have a transparent pricing, easy and straightforward development process
         </p>
-        <Image src={Top} className='absolute right-0 top-0' />
       </div>
 
       {/* //Our Product// */}
       <div className='mt-40'>
-        <p className='text-white text-center font-poppins text-sm not-italic font-semibold leading-normal'>Our Product</p>
-        <div className='flex gap-8'>
-          <div>
-            <div className='bg-white w-1/2 flex gap-4'>
-              <Image src={Minimo} />
+        <p className='text-white text-center font-poppins text-2xl not-italic font-semibold leading-normal mb-8'>Our Product</p>
+        <div className='flex gap-4 m-auto'>
+          <div className='grid gap-y-6'>
+            <div className='bg-white w-1/2 flex gap-4 px-10 py-12 m-auto rounded-lg'>
+              <Image src={Minimo} className='w-16 h-16'/>
               <div>
-                <p>Mobile Apps</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <p className='text-black text-left font-poppins text-2xl not-italic font-semibold leading-normal'>Mobile Apps</p>
+                <p className='text-black text-left font-poppins text-sm not-italic font-normal leading-normal'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
               </div>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                <path d="M13.75 3.75L18.75 10M18.75 10L13.75 16.25M18.75 10H1.25" stroke="#FFB347" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
-            </div>
-            <div className='bg-black w-1/2 flex gap-4'>
-              <Image src={Minip} />
-              <div>
-                <p>Website</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-              </div>
-            </div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
               <path d="M13.75 3.75L18.75 10M18.75 10L13.75 16.25M18.75 10H1.25" stroke="#FFB347" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
+            </div>
+            <div className='bg-black w-5/12 flex gap-4 px-5 py-12 m-auto border border-gray-400 rounded-lg'>
+              <Image src={Minip} className='w-16 h-16' />
+              <div>
+                <p className='text-white text-left font-poppins text-2xl not-italic font-semibold leading-normal'>Website</p>
+                <p className='text-white text-left font-poppins text-sm not-italic font-normal leading-normal'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+              </div>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+              <path d="M13.75 3.75L18.75 10M18.75 10L13.75 16.25M18.75 10H1.25" stroke="#FFB347" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            </div>
+            
           </div>
-          <div className='bg-white w-1/2'>
-            <div className='flex gap-8'>
+          <div className='bg-white w-1/2 rounded-lg'>
+            <div className='flex gap-8 justify-center'>
               <Image src={Iphone} />
               <Image src={Iphone} />
               <Image src={Iphone} />
             </div>
-            <p>Mobile Apps</p>
-            <p>Mobile apps Android dan iPhone sekaligus, buat aplikasi dengan fitur sesuka anda.
+            <div className='p-10'>
+            <p className='text-black text-center font-poppins text-2xl not-italic font-semibold leading-normal mb-8'>Mobile Apps</p>
+            <p className='textblack text-center font-poppins text-base not-italic font-normal leading-normal mb-8'>Mobile apps Android dan iPhone sekaligus, buat aplikasi dengan fitur sesuka anda.
               Bisa untuk aplikasi transaksi jual beli, Aplikasi kasir, Aplikasi Informasi, maupun Aplikasi chat dengan fitur GPS.</p>
             <Button
               label="Pelajari Selengkapnya"
               // loading={loading || disabled}
-              className={'w-44 pr-5 pl-5 pt-3 pb-3 mt-2.5 mb-8 rounded-full bg-teal-400 text-white'}
+              className={'w-1/2 px-4 pt-3 pb-3 mt-2.5 mb-8 rounded-full bg-teal-400 text-white font-normal m-auto'}
             />
+            </div>
           </div>
         </div>
       </div>
 
       {/* //Testimony// */}
-      <div>
-        <p className='text-white text-center font-poppins text-sm not-italic font-semibold leading-normal'>Testimony</p>
-        <div className="flex gap-8 mb-12 flex-nowrap">
+      <div className='m-auto'>
+        <p className='text-white text-left font-poppins text-2xl not-italic font-semibold leading-normal mb-6'>Testimony</p>
+        <div className="scroll-pad flex gap-8 mb-12 flex-nowrap overflow-scroll">
           {data.result.map((item) => {
             return <MiniCards
               id={item.id}
@@ -140,49 +141,49 @@ const Home = () => {
       </div>
 
       {/* //Our Work// */}
-      <div>
-        <p>Our Works</p>
-        <div className='flex flex-nowrap'>
+      <div className='mb-16'>
+        <p className='text-white text-center font-poppins text-2xl not-italic font-semibold leading-normal mb-8'>Our Works</p>
+        <div className='flex flex-nowrap gap-x-8 rounded-2xl ml-16'>
           <div className='our-blue'>
-            <div className='flex gap-10'>
-              <div className='img-tosca'></div>
-              <div>
-                <p>Rayu Motor</p>
-                <p>AC Service Workshop that has implemented online registration to make it easier for consumers, mechanics, and sales</p>
+            <div className='flex gap-5 my-24'>
+              <div className='img-tosca ml-4'></div>
+              <div className='text-white font-poppins not-italic pr-5'>
+                <p className='text-4xl text-center font-semibold leading-semibold pt-2'>Rayu Motor</p>
+                <p className='text-xl text-left font-normal leading-normal pt-1'>AC Service Workshop that has implemented online registration to make it easier for consumers, mechanics, and sales</p>
               </div>
             </div>
           </div>
           <div className='our-orange'>
-            <div className='img-orange'></div>
+            <div className='img-orange ml-4 my-16'></div>
           </div>
         </div>
       </div>
 
       {/* //How We Work// */}
       <div className='how-blue mb-11'>
-        <p className='text-white text-center font-poppins text-2xl not-italic font-semibold leading-normal mb-8'>How We Work</p>
-        <div className='how-black flex gap-10 m-auto'>
-          <div className='grid gap-10  bg-white'>
-            <div className='flex gap-10 bg-grey'>
+        <p className='text-white text-center font-poppins text-2xl not-italic font-semibold leading-normal my-8'>How We Work</p>
+        <div className='how-black flex gap-10 m-auto mt-9 mb-32'>
+          <div className='bg-white my-7 ml-7 rounded-xl'>
+            <div className='flex gap-x-3.5 gap-y-3 bg-grey m-auto my-6'>
               <Image src={People} className='w-9 h-9'/>
-              <p>My name is Danny, I’m looking for profile website company</p>
+              <p className='bg-slate-200 py-1.5 px-2.5 text-black items-justify font-poppins text-xs not-italic font-normal leading-normal mr-4'>My name is Danny, I’m looking for profile website company</p>
             </div>
-            <div className='flex gap-10 bg-grey'>
-              <p>Hello Danny, thank to reaching out us. We are interested to your offer. Maybe we can set meeting</p>
-              <Image src={Cube} className='w-9 h-9'/>
+            <div className='flex gap-x-3.5 gap-y-3 bg-grey m-auto my-6'>
+              <p className='bg-slate-200 py-1.5 px-2.5 text-black items-justify font-poppins text-xs not-italic font-normal leading-normal ml-4'>Hello Danny, thank to reaching out us. We are interested to your offer. Maybe we can set meeting</p>
+              <Image src={Cube} className='w-9 h-9 mr-4'/>
             </div>
-            <div className='flex gap-10 bg-grey'>
-              <Image src={People} className='w-9 h-9'/>
-              <p>Sounds good, I have many desires to explain</p>
+            <div className='flex gap-x-3.5 gap-y-3 bg-grey m-auto my-6'>
+              <Image src={People} className='w-9 h-9 ml-4'/>
+              <p className='bg-slate-200 py-1.5 px-2.5 text-black items-justify font-poppins text-xs not-italic font-normal leading-normal mr-4'>Sounds good, I have many desires to explain</p>
             </div>
-            <div className='flex gap-10 bg-grey'>
-              <p>How do I arrange our meeting on Monday at 10 WIB and my team will contact you again</p>
-              <Image src={Cube} className='w-9 h-9'/>
+            <div className='flex gap-x-3.5 gap-y-3 bg-grey m-auto my-6'>
+              <p className='bg-slate-200 py-1.5 px-2.5 text-black items-justify font-poppins text-xs not-italic font-normal leading-normal ml-4'>How do I arrange our meeting on Monday at 10 WIB and my team will contact you again</p>
+              <Image src={Cube} className='w-9 h-9  mr-4'/>
             </div>
           </div>
-          <div>
-            <p>Estimate</p>
-            <p>Estimate the cost of creating your application with us. Pricing is transparent at the start with a gradual payment method.</p>
+          <div className='text-white text-left font-poppins text-xl not-italic my-7 mr-7'>
+            <p className='font-semibold leading-semibold mb-4'>Estimate</p>
+            <p className='font-normal leading-normal'>Estimate the cost of creating your application with us. Pricing is transparent at the start with a gradual payment method.</p>
           </div>
         </div>
         <div>
